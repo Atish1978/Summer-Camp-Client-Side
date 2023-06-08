bcd4dfc605225ea50f957572c69531c2a35ec472
Atish Dipankar Chakraborty <atishanindya@gmail.com>
2023-06-08 21:26:38 +0600

    Pupular Classess Sorted

--- src/pages/PopularClasses/PopularClasses.jsx
+++ src/pages/PopularClasses/PopularClasses.jsx
@@ -4,22 +4,32 @@ import PopularClassesCard from "./PopularClassesCard";
 
 
 const PopularClasses = () => {
-    const [classes, SetClasses]=useState([]);
+    const [data, SetData]=useState([]);
 
     useEffect(()=>{
         fetch('classes.json')
         .then(res=>res.json())
         .then(data=>{
-            SetClasses(data.slice(0, 6));
+            SetData(data);
         })
     } , [])
+
+    const compareClasses = (a, b) => {
+        const maxStudentsA = a.studentsNumber;
+        const maxStudentsB = b.studentsNumber;
+        return maxStudentsB - maxStudentsA;
+      };
+    
+      const sortedData = [...data].sort(compareClasses).slice(0, 6);
+
+
     return (
         <div className="my-6">
         <div className="text-center">
             <h3 className='text-5xl'>Popular Classes</h3>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto gap-4">
                 {
-                    classes.map(pclass=><PopularClassesCard
+                    sortedData.map(pclass=><PopularClassesCard
                     key={pclass._id}
                     pclass={pclass}></PopularClassesCard>)
                 }
